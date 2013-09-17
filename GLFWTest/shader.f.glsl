1dd7dd8f84ff80822ddabc2082ed8f17124fefac
Jacopo Santoni <jack@pixbits.com>
2013-09-17 00:27:12 +0000

    prove specular lighting

--- GLFWTest/shader.f.glsl
+++ GLFWTest/shader.f.glsl
@@ -23,7 +23,7 @@ float rand(vec2 co){
 void main() {
   mat4 mvMatrix = vMatrix*mMatrix;
   
-  vec3 lightColor = vec3(0.9, 0.9, 0.6);
+  vec3 lightColor = vec3(1.0f,1.0f,1.0f);
   vec3 lightPosition = vec3(mvMatrix * vec4(0,0,3,1));
   
   vec3 surfacePos = vec3(mMatrix * vec4(v_position, 1.0f));
@@ -31,7 +31,17 @@ void main() {
   vec3 surfaceToLight = normalize(lightPosition - surfacePos);
   float diffuseCoefficient = max(0.0, dot(v_normal, surfaceToLight));
   
-  fragColor = vec4(diffuseCoefficient * surfaceColor.rgb /** lightColor*/, 1);
+  vec3 incidenceVector = -surfaceToLight;
+  vec3 reflectionVector = reflect(incidenceVector, v_normal);
+  vec3 surfaceToCamera = normalize(vec3(0,0,1) - surfacePos);
+  float cosAngle = max(0.0, dot(surfaceToCamera, reflectionVector));
+  float specularCoefficient = pow(cosAngle, 0.4);
+  
+  vec3 specularComponent = specularCoefficient*vec3(0.9,0.9,1.0);
+  
+  vec3 combinedLight = vec3(0.2,0.2,0.2) + specularComponent /*+ diffuseCoefficient*lightColor*/;
+
+  fragColor = vec4((specularCoefficient + diffuseCoefficient * lightColor)*surfaceColor.rgb, 1);
   
   //fragColor = vec4(texture(tex, v_texCoord).rgb, v);
 }
